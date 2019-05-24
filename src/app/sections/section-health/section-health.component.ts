646d012b921b7ed26d054d3f114ce1ad3e2376bd
GermanEngineer <kubica.mathis@gmail.com>
2019-05-24 11:10:20 +0200

    Created sample data version of line chart

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Server} from '../../shared/server';
+
+const SAMPLE_SERVERS =[
+  {
+    id: 1,
+    name: 'Server01',
+    isOnline: true
+  },
+  {
+    id: 2,
+    name: 'Server02',
+    isOnline: false
+  },
+  {
+    id: 3,
+    name: 'Server03',
+    isOnline: true
+  },
+  {
+    id: 4,
+    name: 'Server04',
+    isOnline: true
+  }
+];
 
 @Component({
   selector: 'app-section-health',
@@ -9,7 +33,10 @@ export class SectionHealthComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  servers: Server[] = SAMPLE_SERVERS;
+  
+  ngOnInit() { 
   }
 
 }
+;
